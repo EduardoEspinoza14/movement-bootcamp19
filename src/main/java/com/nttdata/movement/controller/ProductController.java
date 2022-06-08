17255988536557611eedbc88da35741bf4a93619
EduardoEspinoza14 <eduardoantonio.espinozallontop@everis.nttdata.com>
2022-06-08 17:50:45 -0500

    Commit

---
+++ src/main/java/com/nttdata/movement/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.nttdata.movement.controller;
+
+import com.nttdata.movement.bussiness.ProductService;
+import com.nttdata.movement.model.dto.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/product")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
+public class ProductController {
+
+    private final Logger log = LoggerFactory.getLogger(EmployeeController.class);
+
+    @Autowired
+    ProductService service;
+
+    @PostMapping(value = "/available-balance/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Double> availableBalance(@PathVariable String customerId, @PathVariable String productId){
+        return service.getAvailableBalance(customerId, productId);
+    }
+
+}
